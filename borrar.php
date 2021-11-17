4400dd15a078650e9379145693bb8b0c5048976a
Alf0nMV <alfonsomolina43@gmail.com>
2021-11-17 13:21:44 +0100

    commit

---
+++ borrar.php
@@ -0,0 +1,43 @@
+<?php
+
+include 'head.php';
+session_start();
+if(isset($_REQUEST['insertar']))
+{
+  $matricula=$_REQUEST['matricula'];
+
+  $codigo=$_REQUEST['codigo'];
+  unset($_SESSION['insertar'][$codigo]);
+
+}
+
+echo' 
+Introduce los datos de la Multa a Borrar<mark>(1.5 Puntos)<br><br>
+                         
+<div   class="postcontent"><form action="borrar.php" method="post">
+<table align="center" class="content-layout">
+  
+  
+  <tr>
+  <td align="right">
+  <strong>Matricula :</strong></td><td>
+  <div align="left">
+        <input type="text" name="matricula" size="10"/>
+  </div>
+  </td>
+  </tr>
+  <tr>
+    <td align="right"><strong>Fecha y Hora :</strong></td>
+     <td>
+        <input  type="datetime-local" name="fecha_hora" size="20" />
+     </td>
+  </tr>
+  
+  <tr ><td colspan="2"><div align="center"><strong>
+<input name="borrar" type="submit"  value="Borrar Multa"/>
+</strong></div></td></tr>
+</table>
+</form>
+</div>';        
+include 'pie.php';
+
